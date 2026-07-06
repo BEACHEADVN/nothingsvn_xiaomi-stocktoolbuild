4d3470ca0b2252f0177473ecc060a33ed731e29e
Chau Vinh Tien <tiencv102006@gmail.com>
2026-07-07 03:29:49 +0700

    Update

--- bin/modfile/Universal/gmsservices/update.sh
+++ bin/modfile/Universal/gmsservices/update.sh
@@ -1,4 +1,4 @@
-work_dir=$(pwd)
+﻿work_dir=$(pwd)
 source $work_dir/functions.sh
 rom_os=$(cat $work_dir/bin/ddevice/rom_os.txt)
 androidVER=$(cat $work_dir/bin/ddevice/androidver.txt)
@@ -6,7 +6,7 @@ regionTYPE=$(cat $work_dir/bin/ddevice/device_type.txt)
 MAIN_FOLDER="$work_dir/build/baserom/images"
 
 if [[ $regionTYPE == "China" ]]; then 
-  aria2c_with_github_token -q -d "$work_dir/bin/modfile/Universal/gmsservices/product/priv-app/GoogleVelvet_CTS/" -o GoogleVelvet_CTS.apk https://github.com/tiencv2006/nothingsvn_oplus-toolbuild/releases/download/oplus/GoogleVelvet_CTS.apk && info "Get File Successfully"
+  aria2c -q -d "$work_dir/bin/modfile/Universal/gmsservices/product/priv-app/GoogleVelvet_CTS/" -o GoogleVelvet_CTS.apk https://github.com/tiencv2006/NothingsVN-BuildExt/releases/download/oplus/GoogleVelvet_CTS.apk && info "Get File Successfully"
   cp -rf $work_dir/bin/modfile/Universal/gmsservices/product/* $work_dir/build/baserom/images/product/
   cp -rf $work_dir/bin/modfile/Universal/gmsservices/system_ext/* $work_dir/build/baserom/images/system_ext/
   echo "ro.miui.has_gmscore=1" >> $work_dir/build/baserom/images/system/system/build.prop
