2fb03ecd45bf956aca4051772e8202d1c4f94da4
Chau Vinh Tien <tiencv102006@gmail.com>
2026-07-15 08:35:50 +0700

    Update packROM.sh

--- packROM.sh
+++ packROM.sh
@@ -68,7 +68,7 @@ for pname in ${super_list}; do
         elif [[ "$PACK_TYPE" == "EROFS" ]]; then
             python3 $work_dir/bin/fspatch.py $work_dir/build/baserom/images/${pname} $work_dir/build/baserom/images/config/${pname}_fs_config >/dev/null 2>&1
             python3 bin/contextpatch.py $work_dir/build/baserom/images/${pname} $work_dir/build/baserom/images/config/${pname}_file_contexts >/dev/null 2>&1
-            mkfs.erofs --quiet -zlz4hc,9 --mount-point ${pname} --fs-config-file=$work_dir/build/baserom/images/config/${pname}_fs_config --file-contexts=$work_dir/build/baserom/images/config/${pname}_file_contexts $work_dir/build/baserom/images/${pname}.img $work_dir/build/baserom/images/${pname} >/dev/null 2>&1
+            mkfs.erofs --quiet -zlz4hc,9 --sparse --mount-point ${pname} --fs-config-file=$work_dir/build/baserom/images/config/${pname}_fs_config --file-contexts=$work_dir/build/baserom/images/config/${pname}_file_contexts $work_dir/build/baserom/images/${pname}.img $work_dir/build/baserom/images/${pname} >/dev/null 2>&1
             if [ -f "$work_dir/build/baserom/images/${pname}.img" ]; then
                 repack "Packing [${pname}.img] success"
             else
