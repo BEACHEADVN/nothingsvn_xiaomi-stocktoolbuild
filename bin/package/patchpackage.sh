68bc33350908065291b1582e0b08a0c7f98bc43e
tiencv2006 <tiencv102006@gmail.com>
2026-07-16 23:42:13 +0700

    Update NotificationFix

--- bin/package/patchpackage.sh
+++ bin/package/patchpackage.sh
@@ -6,4 +6,5 @@ target_dir="$work_dir/bin/package/"
 
 bash $target_dir/DISABLE_AVB/DISABLEavb.sh
 bash $target_dir/RefreshRate/1hz.sh
+bash $target_dir/NOTIFICATION_FIX/notificationFIX.sh
 mods "Add Package Done"
